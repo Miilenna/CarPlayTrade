cae22079db62b02f077b2097a1cdb79fa5127ba0
Miilenna <milena200.fv@gmail.com>
2025-03-05 16:18:40 +0100

    pg principal, header y registro hecho

---
+++ js/header.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('../html/header.html')  // Ruta ajustada
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error al cargar el header:', error);
+        });
+});
